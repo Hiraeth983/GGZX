60171614c90606d54891df3d9e8333fe755bfa7e
Hiraeth983 <2670428853@qq.com>
2024-01-12 19:32:18 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="box">
+        <h2>App根组件</h2>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+.box{
+  height: 600px;
+}
+</style>
